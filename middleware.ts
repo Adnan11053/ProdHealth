8c996f2c8caa5d32f9949b2867c55262709b1456
Adnan Rampurawala <adnan.mr@somaiya.edu>
2025-11-21 21:51:41 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import { createServerClient } from "@supabase/ssr"
 
-export const runtime = "edge";
-
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
 
